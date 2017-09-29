fc53a703113516bb9e87770ea3af1a7f53c72276
drivera2960 <32206095+drivera2960@users.noreply.github.com>
2017-09-29 15:51:00 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String response = scanner.next();
+        System.out.println("I am " + response + " Conquerer of Worlds!");
+    }
+}
